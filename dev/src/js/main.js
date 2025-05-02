f19275b0a959acd63e1ac29aaef5ea1bc80c7d8c
Vadym-K <vadymkolisnichenko89@gmail.com>
2025-05-02 11:55:20 +0300

    fonts header footer

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-	//calls functions
+$(function() {
+	initSortToggle();
+	initBurgerMenu();
+	initMobileSearch();
 });
 
 $(window).on('load', function () {
@@ -19,3 +21,62 @@ $(window).scroll(function() {
 });
 
 //functions
+
+function initSortToggle() {
+	$(".js-drop").on("click", function(e) {
+		e.preventDefault();
+
+		var $dropParent = $(this).closest(".drop");
+
+		if (!$dropParent.hasClass("open-drop")) {
+			$(".drop").removeClass("open-drop");
+			$dropParent.addClass("open-drop");
+		} else {
+			$dropParent.removeClass("open-drop");
+		}
+	});
+
+	$(document).on("click", function(e) {
+		if (!$(e.target).closest(".drop").length) {
+			$(".drop").removeClass("open-drop");
+		}
+	});
+}
+
+function initBurgerMenu() {
+	$(".burger").on("click", function(e) {
+		e.stopPropagation();
+
+		$(".header-top").removeClass("open-search");
+		$(this).closest(".header-top").toggleClass("open-menu");
+
+		if ($(".user-profile.drop").hasClass("open-drop")) {
+			$(".user-profile.drop").removeClass("open-drop");
+		}
+	});
+
+	$(document).on("click", function(e) {
+		if (!$(e.target).closest(".burger").length) {
+			$(".header-top").removeClass("open-menu");
+		}
+	});
+}
+
+function initMobileSearch() {
+	$(".btn-search").on("click", function(e) {
+		e.stopPropagation();
+
+		$(".header-top").removeClass("open-menu");
+		$(this).closest(".header-top").toggleClass("open-search");
+	});
+
+	$(document).on("click", function(e) {
+		if ($(e.target).closest(".wrap-search").length) {
+			return;
+		}
+
+		if (!$(e.target).closest(".btn-search").length) {
+			$(".header-top").removeClass("open-search");
+		}
+	});
+}
